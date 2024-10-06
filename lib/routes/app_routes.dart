97fdd6606cb6ce196f3130970a47cf40eb8c82b0
SHAFIRA AUREL AZZAHRA <163809180+ShafiraAurelAzzahra@users.noreply.github.com>
2024-10-06 10:56:11 +0700

    Update app_routes.dart

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,68 +1,49 @@
-import 'package:get/get.dart'; 
-// Mengimpor GetX untuk manajemen state, routing, dan dependency injection.
+import 'package:get/get.dart'; // Mengimpor package GetX untuk manajemen state dan navigasi
+import '../tampilan_depan_controller.dart'; // Mengimpor controller untuk tampilan depan
+import '../tampilan_depan_screen.dart'; // Mengimpor widget tampilan depan
+import '../home_controller.dart'; // Mengimpor controller untuk home screen
+import '../home_screen.dart'; // Mengimpor widget home screen
+import '../materi.dart'; // Mengimpor widget materi
+import '../quiz.dart'; // Mengimpor widget quiz
 
-import '../tampilan_depan_controller.dart'; 
-// Mengimpor controller untuk halaman tampilan depan.
-import '../tampilan_depan_screen.dart'; 
-// Mengimpor tampilan untuk halaman tampilan depan.
+class AppRoutes { // Mendeklarasikan kelas AppRoutes
+  static const String tampilanDepanScreen = '/tampilan_depan_screen'; // Mendefinisikan rute untuk tampilan depan
 
-import '../home_controller.dart'; 
-// Mengimpor controller untuk halaman home.
-import '../home_screen.dart'; 
-// Mengimpor tampilan untuk halaman home.
+  static const String homeScreen = '/home_screen'; // Mendefinisikan rute untuk home screen
 
-class AppRoutes {
-  // Kelas yang berisi daftar route dan navigasi aplikasi.
+  static const String materiPage = '/materi_Page'; // Mendefinisikan rute untuk materi page
 
-  static const String tampilanDepanScreen = '/tampilan_depan_screen'; 
-  // Mendefinisikan route untuk halaman Tampilan Depan.
+  static const String quizPage = '/quiz_Page'; // Mendefinisikan rute untuk quiz page
 
-  static const String homeScreen = '/home_screen'; 
-  // Mendefinisikan route untuk halaman Home.
+  static const String homeinitialPage = '/home_initial_page'; // Rute untuk halaman awal home
 
-  static const String homeinitialPage = '/home_initial_page'; 
-  // Mendefinisikan route untuk halaman awal Home (belum digunakan dalam daftar `pages`).
+  static const String appNavigationScreen = '/app_navigation_screen'; // Rute untuk halaman navigasi aplikasi
 
-  static const String appNavigationScreen = '/app_navigation_screen'; 
-  // Mendefinisikan route untuk navigasi aplikasi (belum digunakan dalam daftar `pages`).
+  static const String initialRoute = '/initialRoute'; // Rute untuk jalur awal
 
-  static const String initialRoute = '/initialRoute'; 
-  // Mendefinisikan route awal aplikasi, yaitu tampilan depan (TampilanDepanScreen).
-
-  static List<GetPage> pages = [
-    // Daftar `GetPage` yang berisi route-route yang akan digunakan untuk navigasi dalam aplikasi.
-
-    GetPage(
-      name: tampilanDepanScreen, 
-      // Route name untuk halaman Tampilan Depan.
-
-      page: () => TampilanDepanScreen(), 
-      // Fungsi yang mengembalikan instance dari TampilanDepanScreen.
-
-      bindings: [TampilanDepanBinding()], 
-      // Mendefinisikan binding yang digunakan untuk halaman ini, mengatur dependency injection untuk `TampilanDepanScreen`.
+  static List<GetPage> pages = [ // Mendeklarasikan list yang berisi rute-rute aplikasi
+    GetPage( // Rute untuk tampilan depan
+      name: tampilanDepanScreen, // Nama rute
+      page: () => TampilanDepanScreen(), // Widget yang akan ditampilkan
+      bindings: [TampilanDepanBinding()], // Binding untuk controller yang terkait
     ),
-
-    GetPage(
-      name: homeScreen, 
-      // Route name untuk halaman Home.
-
-      page: () => HomeScreen(), 
-      // Fungsi yang mengembalikan instance dari HomeScreen.
-
-      bindings: [HomeBinding()], 
-      // Mendefinisikan binding yang digunakan untuk halaman ini, mengatur dependency injection untuk `HomeScreen`.
+    GetPage( // Rute untuk home screen
+      name: homeScreen, // Nama rute
+      page: () => HomeScreen(), // Widget yang akan ditampilkan
+      bindings: [HomeBinding()], // Binding untuk controller yang terkait
     ),
-
-    GetPage(
-      name: initialRoute, 
-      // Route name untuk route awal aplikasi.
-
-      page: () => TampilanDepanScreen(), 
-      // Mengembalikan instance dari TampilanDepanScreen sebagai halaman awal aplikasi.
-
-      bindings: [TampilanDepanBinding()], 
-      // Binding yang digunakan untuk halaman awal aplikasi.
+    GetPage( // Rute untuk materi page
+      name: materiPage, // Nama rute
+      page: () => MateriPage(), // Widget yang akan ditampilkan
     ),
+    GetPage( // Rute untuk quiz page
+      name: quizPage, // Nama rute
+      page: () => QuizPage(), // Widget yang akan ditampilkan
+    ),
+    GetPage( // Rute untuk jalur awal
+      name: initialRoute, // Nama rute
+      page: () => TampilanDepanScreen(), // Widget yang akan ditampilkan
+      bindings: [TampilanDepanBinding()], // Binding untuk controller yang terkait
+    )
   ];
 }
